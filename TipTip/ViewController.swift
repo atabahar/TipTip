a5574a11a620a4f1d53e31234d28fc0dcd2ee2a7
Ata Bahar <AtaBahar@atas-macbook-pro.local>
2019-01-27 20:43:05 -0800

    Initial Commit

---
+++ TipTip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipTip
+//
+//  Created by Ata Bahar on 1/27/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
